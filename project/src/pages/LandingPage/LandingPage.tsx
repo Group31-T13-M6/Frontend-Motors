e0b6a800ebfcdb6ac07750af33dfb6d82684cc82
Rikellyh <rikellyh898@gmail.com>
2023-04-10 16:25:21 -0300

    feat: header, footer

--- project/src/pages/LandingPage/LandingPage.tsx
+++ project/src/pages/LandingPage/LandingPage.tsx
@@ -1,10 +1,16 @@
-import Text from "src/styles/typography";
+import Footer from 'src/components/Footer/Footer';
+import HeaderNav from 'src/components/Header/Header';
+import Text from 'src/styles/typography';
 
 const LandingPage = () => {
   return (
-    <Text tag="h2" fontSize="title-3-500" color="brand1">
-      Landing Page
-    </Text>
+    <>
+      <HeaderNav />
+      <Text tag='h2' fontSize='title-3-500' color='brand1'>
+        Landing Page
+      </Text>
+      <Footer />
+    </>
   );
 };
 
