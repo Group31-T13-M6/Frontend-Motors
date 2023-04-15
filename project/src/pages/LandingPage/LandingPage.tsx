e82039490d9e7942d4e93b5feef4c4a6366115ae
Gabriel Malafaia <gabrielbsnsp@gmail.com>
2023-04-15 11:20:44 -0300

    fix: font styles and backgroud initial image

--- project/src/pages/LandingPage/LandingPage.tsx
+++ project/src/pages/LandingPage/LandingPage.tsx
@@ -1,6 +1,5 @@
 import Sidebar from "src/components/Sidebar/Sidebar";
 import Text from "src/styles/typography";
-import PhotoHome from "../../assets/photoHome.png";
 import ReactPaginate from "react-paginate";
 import HeaderNav from "src/components/Header/Header";
 import Footer from "src/components/Footer/Footer";
@@ -10,33 +9,34 @@ import { Section, Main, Separator } from "./styles";
 const LandingPage = () => {
   return (
     <div>
-      <HeaderNav/>
-      
-      <Section>
-        <div className="intro-img">
-          <img src={PhotoHome} alt="Carro da BMW"/>
-        </div>
+      <HeaderNav />
 
+      <Section>
         <div className="intro-text">
-          <Text tag="h1" fontSize="title-2-600" color="grey10">Motors Shop</Text>
-          <Text tag="p" fontSize="title-5-600" color="grey10">A melhor plataforma de anúncios de carros do país</Text>
+          <Text tag="h1" fontSize="title-2-600" color="grey10">
+            Motors Shop
+          </Text>
+          <Text tag="p" fontSize="title-5-600" color="grey10">
+            A melhor plataforma de anúncios de carros do país
+          </Text>
         </div>
       </Section>
 
       <Separator>
-        <Sidebar/>
+        <Sidebar />
 
         <Main>
-          <Card/>
+          <Card />
 
           <ReactPaginate
-          previousLabel={""}
-          nextLabel = {"Seguinte >"}
-          pageCount={1}/>
+            previousLabel={""}
+            nextLabel={"Seguinte >"}
+            pageCount={1}
+          />
         </Main>
       </Separator>
 
-      <Footer/>
+      <Footer />
     </div>
   );
 };
