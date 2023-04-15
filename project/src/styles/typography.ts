ee7f2d0038454ddbcfcc18799fa31e70b2c2f75e
Gabriel Malafaia <gabrielbsnsp@gmail.com>
2023-04-15 09:56:07 -0300

    fix: styles font size

--- project/src/styles/typography.ts
+++ project/src/styles/typography.ts
@@ -8,126 +8,136 @@ const Text = styled(TitleComponent)`
     switch (fontSize) {
       case "title-1-700":
         return css`
-          font-size: 4.4rem;
+          font-size: 2.75rem;
           font-weight: 700;
           font-family: var(--font-family1);
         `;
       case "title-2-600":
         return css`
-          font-size: 3.6rem;
+          font-size: 2.25rem;
           font-weight: 600;
           font-family: var(--font-family1);
         `;
       case "title-3-600":
         return css`
-          font-size: 3.2rem;
+          font-size: 2rem;
           font-weight: 600;
           font-family: var(--font-family1);
         `;
       case "title-3-500":
         return css`
-          font-size: 3.2rem;
+          font-size: 2rem;
           font-weight: 500;
           font-family: var(--font-family1);
         `;
       case "title-4-600":
         return css`
-          font-size: 2.8rem;
+          font-size: 1.75rem;
           font-weight: 600;
           font-family: var(--font-family1);
         `;
       case "title-4-500":
         return css`
-          font-size: 2.8rem;
+          font-size: 1.75rem;
           font-weight: 500;
           font-family: var(--font-family1);
         `;
       case "title-5-600":
         return css`
-          font-size: 2.4rem;
+          font-size: 1.5rem;
           font-weight: 600;
           font-family: var(--font-family1);
         `;
       case "title-5-500":
         return css`
-          font-size: 2.4rem;
+          font-size: 1.5rem;
           font-weight: 500;
           font-family: var(--font-family1);
         `;
       case "title-6-600":
         return css`
-          font: var(--Heading-6-600);
+          font-size: 1.25rem;
+          font-weight: 600;
+          font-family: var(--font-family1);
         `;
       case "title-6-500":
         return css`
-          font: var(--Heading-6-500);
+          font-size: 1.25rem;
+          font-weight: 500;
+          font-family: var(--font-family1);
         `;
       case "title-7-600":
         return css`
-          font: var(--Heading-7-600);
+          font-size: 1rem;
+          font-weight: 600;
+          font-family: var(--font-family1);
         `;
       case "title-7-500":
         return css`
-          font: var(--Heading-7-500);
+          font-size: 1rem;
+          font-weight: 500;
+          font-family: var(--font-family1);
         `;
       case "body-1-400":
         return css`
-          font-size: 1.6rem;
+          font-size: 1rem;
           font-weight: 400;
           font-family: var(--font-family2);
         `;
 
       case "body-1-600":
         return css`
-          font-size: 1.6rem;
+          font-size: 1rem;
           font-weight: 600;
           font-family: var(--font-family2);
         `;
 
       case "body-2-400":
         return css`
-          font-size: 1.4rem;
+          font-size: 0.875rem;
           font-weight: 400;
           font-family: var(--font-family2);
         `;
 
       case "body-2-500":
         return css`
-          font: var(--body-2-500);
+          font-size: 0.875rem;
+          font-weight: 500;
+          font-family: var(--font-family2);
         `;
 
       case "button-big-text":
         return css`
-          font-size: 1.6rem;
+          font-size: 1rem;
           font-weight: 600;
           font-family: var(--font-family2);
         `;
 
       case "button-medium-text":
         return css`
-          font-size: 1.4rem;
+          font-size: 0.875rem;
           font-weight: 600;
           font-family: var(--font-family2);
         `;
 
       case "input-placeholder":
         return css`
-          font-size: 1.4rem;
+          font-size: 1rem;
           font-weight: 400;
           font-family: var(--font-family2);
         `;
 
       case "input-label":
         return css`
-          font-size: 1.4rem;
+          font-size: 0.875rem;
           font-weight: 500;
           font-family: var(--font-family2);
         `;
       case undefined:
         return css`
-          font-size: 1.6rem;
-          font-weight: 500;
-          font-family: var(--font-family1);
+          font-size: 1rem;
+          font-weight: 600;
+          font-family: var(--font-family2);
         `;
     }
   }}
