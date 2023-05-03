0fbc0c4f39e63479541362d09545c311c6527ae1
Gabriel Malafaia <gabrielbsnsp@gmail.com>
2023-05-03 10:11:29 -0300

    fix: bugs in card

--- project/src/components/Card/index.tsx
+++ project/src/components/Card/index.tsx
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import { IUserRequestAnnouncements, iProduct } from "src/context/HomeContext";
 import Text from "src/styles/typography";
 import CardComponent from "./style";
 import { formatBRL, formatInitialName } from "src/services/helpers";
 import { useNavigate } from "react-router-dom";
 import { StyledInitialName } from "src/styles/components/StyledInitialName";
-=======
-import { iProduct } from 'src/context/HomeContext';
-import Text from 'src/styles/typography';
-import CardComponent from './style';
-import { formatBRL } from 'src/services/helpers';
-import { useNavigate } from 'react-router-dom';
-import { StyledInitialName } from 'src/styles/components/StyledInitialName';
->>>>>>> 2dfea156ec2cfc9e7290a8655165bad84719b9d0
 
 const Card = ({
   id,
@@ -26,47 +17,28 @@ const Card = ({
 }: iProduct) => {
   const navigate = useNavigate();
 
-<<<<<<< HEAD
   const handleRedirect = () => {
     navigate(`/produto/${id}`);
   };
-=======
-  const userName = user ? user.name : 'Unknown';
->>>>>>> 2dfea156ec2cfc9e7290a8655165bad84719b9d0
 
   return (
     <CardComponent onClick={handleRedirect}>
       <div>
-<<<<<<< HEAD
         <img className="car-image" src={images && images[0].url} alt="" />
         <div className="car-mainInfo">
           <Text tag="h2" fontSize="title-7-600" color="grey1">
-=======
-        <img className='car-image' src={images[0].url} alt='' />
-        <div className='car-mainInfo'>
-          <Text tag='h2' fontSize='title-7-600' color='grey1'>
->>>>>>> 2dfea156ec2cfc9e7290a8655165bad84719b9d0
             {brand}
           </Text>
           <Text tag='p' fontSize='body-2-400' color='grey2'>
             {description}
           </Text>
         </div>
-<<<<<<< HEAD
         <div className="car-advertiser">
           <StyledInitialName w="30" h="30">
             {user?.name && user.name[0]}
           </StyledInitialName>
           <Text tag="p" fontSize="body-2-500" color="grey2">
             {user?.name}
-=======
-        <div className='car-advertiser'>
-          <StyledInitialName w='30' h='30'>
-            {userName[0]}
-          </StyledInitialName>
-          <Text tag='p' fontSize='body-3-500' color='grey2'>
-            {userName}
->>>>>>> 2dfea156ec2cfc9e7290a8655165bad84719b9d0
           </Text>
         </div>
         <div className='car-lowInfo'>
